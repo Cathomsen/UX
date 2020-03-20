2f60137e88ad4f87a6dcac5c1ff0526c807898fe
Ida kronborg <55064627+Idakronborgn@users.noreply.github.com>
2020-03-20 16:01:56 +0100

    forside
    kode til forsiden

---
+++ js/script.js
@@ -0,0 +1,142 @@
+
+$(".drop")
+  .mouseover(function() {
+  $(".dropdown").show(300);
+});
+$(".drop")
+  .mouseleave(function() {
+  $(".dropdown").hide(300);     
+});
+
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let selectYear = document.getElementById("year");
+let selectMonth = document.getElementById("month");
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+let monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+
+function next() {
+    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+    showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+    currentYear = parseInt(selectYear.value);
+    currentMonth = parseInt(selectMonth.value);
+    showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+
+    let firstDay = (new Date(year, month)).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+
+    let tbl = document.getElementById("calendar-body"); // body of the calendar
+
+    // clearing all previous cells
+    tbl.innerHTML = "";
+
+    // filing data about month and in the page via DOM.
+    monthAndYear.innerHTML = months[month] + " " + year;
+    selectYear.value = year;
+    selectMonth.value = month;
+
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        // creates a table row
+        let row = document.createElement("tr");
+
+        //creating individual cells, filing them up with data.
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth) {
+                break;
+            }
+
+            else {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode(date);
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                    cell.classList.add("bg-info");
+                } // color today's date
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+
+
+        }
+
+        tbl.appendChild(row); // appending each row into calendar body.
+    }
+
+}
+
+//daysInMonth addEventListener("click",)
+
+//calender ends
+
+//instafeed starts
+
+/*new InstagramFeed({
+
+    'username': '',
+
+    'container': "#instagram-feed-demo",
+
+    'display_profile': false,
+
+    'display_biography': false,
+
+    'display_gallery': true,
+
+    'display_igtv': false,
+    
+    'items': 6,
+    
+    'items_per_row': 3,
+    
+    'margin': 0.5,
+    
+    'image_size': 480,
+    
+    'get_raw_json': true,
+    
+    'styling': false
+
+    'callback': function(data) {
+
+      // ...
+        
+
+        
+    }
+
+    
+    
+    
+    
+    
+
+});
+
+*/
+
